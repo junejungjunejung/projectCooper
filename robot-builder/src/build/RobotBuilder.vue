1dc86abd6449f76c1e6e616b77e714c6b116cd1f
June Jung <costalgia@gmail.com>
2020-04-05 22:27:14 -0700

    components created

--- robot-builder/src/build/RobotBuilder.vue
+++ robot-builder/src/build/RobotBuilder.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div class="content">
+    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
       <div class="top part">
         <div class="robot-name">
           {{selectedRobot.head.title}}
+          <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
         </div>
         <img :src="selectedRobot.head.src" title="head"/>
         <button @click="selectPreviousHead()" class="prev-selector">&#9668;</button>
@@ -34,11 +36,29 @@
         <button @click="selectNextBase()" class="next-selector">&#9658;</button>
       </div>
     </div>
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot,index) in cart" :key="index">
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{robot.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length){
   const deprecatedIndex = index - 1;
@@ -55,6 +75,7 @@ export default {
   data() {
     return {
       availableParts,
+      cart: [],
       selectedHeadIndex: 0,
       selectedLeftArmIndex: 0,
       selectedTorsoIndex: 0,
@@ -62,6 +83,7 @@ export default {
       selectedBaseIndex: 0,
     };
   },
+  mixins: [createdHookMixin],
   computed: {
     selectedRobot(){
       return {
@@ -74,6 +96,16 @@ export default {
     }
   },
   methods: {
+    addToCart(){
+      const robot= this.selectedRobot;
+      const cost= robot.head.cost+
+      robot.leftArm.cost+
+      robot.torso.cost+
+      robot.rightArm.cost+
+      robot.base.cost;
+      this.cart.push(Object.assign({}, robot, {cost}))
+    },
+
     selectNextHead(){
       this.selectedHeadIndex = getNextValidIndex(this.selectedHeadIndex, availableParts.heads.length);
     },
@@ -112,15 +144,17 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .part {
   position: relative;
   width:165px;
   height:165px;
   border: 3px solid #aaa;
 } 
-.part img {
-  width:165px;
+.part {
+  img {
+    width:165px;
+  }
 }
 .top-row {
   display:flex;
@@ -206,5 +240,26 @@ export default {
   top: -25px;
   text-align: center;
   width: 100%;
+}
+.sale{
+  color: red;
+}
+.content{
+  position: relative;
+}
+.add-to-cart{
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 3px;
+  font-style: 16px;
+}
+td, th{
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
+.cost{
+  text-align: right;
 }
 </style>
